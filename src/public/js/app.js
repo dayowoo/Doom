04be18f8c27783a2e6b787ba4dcb4f2df0199008
Dayoung Seon <kkbo5222@gmail.com>
2023-03-10 16:20:08 +0900

    [Nomad] 2.2 SocketIO is amazing

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,5 +1,24 @@
 // Frontend Javascript
+// using Socket.io
 
+const socket = io();
+
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  socket.emit("enter_room", { payload: input.value }, () => {
+    console.log("server is done!");
+  });
+  input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
+
+// using WebSocket
+/*
 const messageList = document.querySelector("ul");
 const messageForm = document.querySelector("#message");
 const nicknameForm = document.querySelector("#nickname");
@@ -49,7 +68,7 @@ function handleMessageSubmit(event) {
 
 nicknameForm.addEventListener("submit", handleNicknameSubmit);
 messageForm.addEventListener("submit", handleMessageSubmit);
-
+*/
 
 
 /*
--- src/server.js
+++ src/server.js
@@ -1,9 +1,6 @@
-import { Socket } from "dgram";
 import express from "express";
 import http from "http";
-import { SocketAddress } from "net";
-import { parse } from "path";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 
 const app = express();
 
@@ -12,36 +9,42 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 // 유저가 /public으로 가면 __dirname + "/pulblic" 보여줌
 app.use("/public", express.static(__dirname + "/public"));
-
-
 // first router
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log("Listeneing on http://localhost:3000");
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({server});
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-function onSocketClose() {
-    console.log("Connected to Browser");
-};
 
+wsServer.on("connection", (socket) => {
+    console.log(socket);
+    socket.on("enter_room", (msg, done) => {
+      console.log(msg);
+      setTimeout(() => {
+        done();
+      }, 10000);
+    });
+  });
+
+
+/*
+import WebSocket from "ws";
 // browser array
 const backSockets = [];
-
-
 // backSocket = FE의 frontSocket과 real-time 소통
 // backSocket : 누가 연결했는지 알 수 있음 & 연결한거를 backSocket이란 이름으로 받음
 wss.on("connection", (backSocket) => {
-    // 브라우저가 연결되면, 배열에 넣어줌
+//     // 브라우저가 연결되면, 배열에 넣어줌
     backSockets.push(backSocket);
     backSocket["nickname"] = "Anon";
     console.log("Connected to Browser");
 
-    // 브라우저가 꺼졌을 때
+//     // 브라우저가 꺼졌을 때
     backSocket.on("close", onSocketClose);
 
-    // 브라우저가 서버에 보낸 메세지 받기
+//     // 브라우저가 서버에 보낸 메세지 받기
     backSocket.on("message", (msg) => {
         const message = JSON.parse(msg);
 
@@ -61,5 +64,7 @@ wss.on("connection", (backSocket) => {
 
     backSocket.send("hello");
 });
+*/
 
-server.listen(3000, handleListen);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
